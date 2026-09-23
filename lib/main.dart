7e1eddf3b6658c6820f85cc554c1fee4c2e9af97
admin <admin@gmail.com>
2026-09-23 03:08:04 -0700

    feat: add animated startup splash screen for Android

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:drift/native.dart';
-import 'package:planact/app/planact_app.dart';
+import 'package:planact/app/startup_splash.dart';
 import 'package:planact/core/database/app_database.dart';
 import 'package:planact/features/commitments/application/commitment_repository.dart';
 
@@ -11,9 +11,22 @@ export 'app/planact_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  runApp(PlanActStartup(repositoryFuture: _initializeApplication()));
+}
+
+Future<CommitmentRepository> _initializeApplication() async {
+  final repositoryFuture = _createRepository();
+  await Future.wait<void>([
+    repositoryFuture.then<void>((_) {}),
+    Future<void>.delayed(const Duration(milliseconds: 1800)),
+  ]);
+  return repositoryFuture;
+}
+
+Future<CommitmentRepository> _createRepository() async {
   final directory = await getApplicationDocumentsDirectory();
   final database = AppDatabase(
     NativeDatabase.createInBackground(File('${directory.path}/planact.sqlite')),
   );
-  runApp(PlanActApp(repository: DriftCommitmentRepository(database)));
+  return DriftCommitmentRepository(database);
 }
